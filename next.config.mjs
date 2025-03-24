89f06ac6ca29b8d496febfdb65a2a05834b63945
Sleuth420 <167057432+Sleuth420@users.noreply.github.com>
2025-03-24 16:57:38 +1100

    Update Next.js configuration to set output format, base path for production, and enable unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/perfect-circle' : '',
+  images: { unoptimized: true },
   eslint: {
     ignoreDuringBuilds: true,
   },
